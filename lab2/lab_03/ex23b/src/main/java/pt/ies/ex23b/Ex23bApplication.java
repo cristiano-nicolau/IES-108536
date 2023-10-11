7e07bf4357186a1e96cd1a0ba32700dc66922a80
cristiano-nicolau <cristianonicolau@ua.pt>
2023-10-11 17:26:10 +0100

    lab 2.3 feito

---
+++ lab2/lab_03/ex23b/src/main/java/pt/ies/ex23b/Ex23bApplication.java
@@ -0,0 +1,13 @@
+package pt.ies.ex23b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex23bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex23bApplication.class, args);
+	}
+
+}
---
+++ lab2/lab_03/ex23b/src/test/java/pt/ies/ex23b/Ex23bApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ies.ex23b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex23bApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
